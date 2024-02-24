209660e118ba52f2e0b754a66bff7630c7eebb57
Bryce Kwon <signer@brycekwon.dev>
2024-02-24 00:42:57 -0800

    Update experience database schema

--- server/models/experiences.js
+++ server/models/experiences.js
@@ -18,16 +18,40 @@ const experienceSchema = mongoose.Schema(
     },
     body:
     {
-      type: String,
-      duration: Number,
+      program: String,
+      location: {
+        country: String,
+        region: String,
+      },
+      duration: {
+        startTime: Date,
+        endTime: Date,
+      },
       description: String,
     },
+    images:
+    {
+      img1:
+      {
+        data: Buffer,
+        contentType: String,
+      },
+      img2:
+      {
+        data: Buffer,
+        contentType: String,
+      },
+      img3:
+      {
+        data: Buffer,
+        contentType: String,
+      }
+    }
   }
 )
 
 const experienceModel = mongoose.model('Experiences', experienceSchema);
 
 // TODO: Add image field
-// TODO: Add location field
 
 export default experienceModel;
