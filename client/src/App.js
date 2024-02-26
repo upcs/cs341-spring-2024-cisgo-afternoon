8b0c3f5f9926bdffe88af21564837a49abff632d
Bryce Kwon <signer@brycekwon.dev>
2024-02-26 14:11:08 -0800

    Add status routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,20 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 
 const Main = lazy(() => import('./layouts/Main.js'));
+
 const Home = lazy(() => import('./pages/Home.js'));
 const AddPin = lazy(() => import("./pages/AddPin.js"));
 const Search = lazy(() => import('./pages/Search.js'));
+
 const Contact = lazy(() => import('./pages/Contact.js'));
 const About = lazy(() => import('./pages/About.js'));
 const Help = lazy(() => import('./pages/Help.js'));
+
 const Login = lazy(() => import('./pages/Login.js'));
+
+const Success = lazy(() => import('./pages/Success.js'));
+const Error = lazy(() => import('./pages/Error.js'));
+const NotFound = lazy(() => import('./pages/NotFound.js'));
 
 const App = () => {
   return (
@@ -23,6 +30,9 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/help" element={<Help />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
