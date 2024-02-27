5b4eddd4b4345f9d0b7563f59a3c028fd30092d4
Alex Hardy <hardya25@up.edu>
2024-02-27 14:10:26 -0800

    created model + route for program; cleaned up routes/experiences.js

--- server/routes/experiences.js
+++ server/routes/experiences.js
@@ -4,28 +4,27 @@ import { Router } from 'express';
 
 import experienceModel from '../models/experiences.js';
 
-const experiencesRouter = Router();
+const experienceRouter = Router();
 
-experiencesRouter.get("/", async function (req, res) {
+experienceRouter.get("/", async function (req, res) {
 	// TODO: GET /
 	// Add support for querystring searching
-	// console.log(req.hostname);
 	const experiences = await experienceModel.find({}).lean();
 	res.json(experiences);
 });
 
-experiencesRouter.get("/:id", async function (req, res) {
-	const experiences = await experienceModel.findById(req.params.id).lean();
-	res.json(experiences);
+experienceRouter.get("/:id", async function (req, res) {
+	const experience = await experienceModel.findById(req.params.id).lean();
+	res.json(experience);
 });
 
-experiencesRouter.post("/", async function(req, res) {
+experienceRouter.post("/", async function(req, res) {
 	// TODO: POST /
 	// Add a new entry to the database
 	res.sendStatus(204);
 });
 
-experiencesRouter.post("/:id",async function(req, res) {
+experienceRouter.post("/:id",async function(req, res) {
 	// TODO: POST /:id
 	// 1. Check that the user is permitted to edit the entry
 	// 2.1. Success: Update the entry.   Inform the user of success.
@@ -33,4 +32,4 @@ experiencesRouter.post("/:id",async function(req, res) {
 	res.sendStatus(204);
 });
 
-export default experiencesRouter;
+export default experienceRouter;
