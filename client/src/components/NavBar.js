23312070a793a54b23a7faf607045ba64ddd04b3
Bryce Kwon <signer@brycekwon.dev>
2024-02-27 20:53:03 -0800

    Fix merge with main

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -14,10 +14,9 @@ const NavBar = () => {
   }
 
   return (
-<<<<<<< HEAD
     <div>
       <div className={`navbar ${filterOpen ? "expanded" : "collapsed"}`}>
-        <ul className="opened">
+        <ul className="nav">
           <li className="expanded">
             <Link to="/">
               <i className="material-icons">home</i>
@@ -70,51 +69,7 @@ const NavBar = () => {
         </ul>
       </div>
       <div className="toggle-button" onClick={handleToggleNavBar}>
-=======
-    <ul className="nav">
-      <li className="expanded">
-        <Link to="/">
-          <i class="material-icons">home</i>
-          Home
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link to="/add">
-          <i class="material-icons">location_on</i>
-          Add a Pin
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link to="/search">
-          <i class="material-icons">search</i>
-          Search
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link to="/contact">
-          <i class="material-icons">mail</i>
-          Contact Us
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link to="/about">
-          <i class="material-icons">diversity_3</i>
-          About Us
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link to="/help">
-          <i class="material-icons">help</i>
-          Help
-        </Link>
-      </li>
-      <li className="expanded">
-        <Link onClick={handleFilterMenu}>
-          Filter
-          <i class="material-icons">{filterOpen ? ("expand_less") : ("expand_more")}</i>
-        </Link>
->>>>>>> remotes/origin/main
-        {filterOpen ? (
+             {filterOpen ? (
           <i className="material-icons">close</i>
         ) : (
           <i className="material-icons">menu</i>
