6ec0e5334c1193c83e84eb329674db5d8c931e63
Bryce Kwon <signer@brycekwon.dev>
2024-03-28 00:39:24 -0700

    Fix experiences database model

--- server/models/experiences.js
+++ server/models/experiences.js
@@ -2,31 +2,28 @@ import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
   meta: {
-    isPublic: Boolean,
-    emailVisible: Boolean,
+    isApproved: Boolean,      // entry has been approved by admin
+    isVisible: Boolean,       // entry is visible to the public
+    contactVisible: Boolean,  // entry contact information is visible to the public
   },
-  name: {
-    first: String,
-    last: String,
-  },
-  affiliation: {
-    position: String,
-    department: String,
-  },
-  contact: {
-    email: String,
-    website: String,
-  },
+  name: String,           // name of entry individual
+  contact: String,        // email of entry individual
+  affiliation: String,    // affiliation to the university [ School, Admin, Library, CSC ]
+  program: String,        // purpose of global experience [ Study Abroad, Teaching Abroad, Research ]
   location: {
-    country: String,
-    region: String,
+    country: String,      // experience country
+    city: String,         // experience city
   },
   duration: {
-    startTime: Date,
-    endTime: Date,
+    ongoing: Boolean,     // entry is still ongoing - NO START OR END DATE REQUIRED
+    startDate: Date,      // entry start date MONTH AND YEAR (YYYY-MM)
+    endDate: Date,        // entry end date MONTH AND YEAR (YYYY-MM)
   },
-  program: String,
-  description: String,
+  external: {
+    institutions: String,   // external institutions worked with
+    partnerships: String,   // external partnersships worked with
+  },
+  description: String,      // description of individual's experience (instructions coming from client)
 });
 
 const model = mongoose.model('experiences', schema);
