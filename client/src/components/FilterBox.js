e304eaf5b55e33f1c2ca3f29d58bd17f5c0f876a
Abhi Akkal <abhiakkal27@gmail.com>
2024-03-28 22:17:47 -0700

    filter and login button are here

--- client/src/components/FilterBox.js
+++ client/src/components/FilterBox.js
@@ -1,21 +1,36 @@
 import React from 'react';
- import '../static/css/components/Filter.css';
+import '../static/css/components/Filter.css';
 
- const FilterBox = () => {
-   return (
-     <div className="filter-box">
-       <h2>Filter</h2>
-       <label>
-         <input type="checkbox" /> International Work
-       </label>
-       <label>
-         <input type="checkbox" /> Volunteer Work
-       </label>
-       <label>
-         <input type="checkbox" /> Study Abroad
-       </label>
-     </div>
-   );
- }
+const FilterBox = () => {
+  return (
+    <div className="filter-box">
+      <h2>Filter</h2>
+      <label>
+        <input type="checkbox" /> Administration
+      </label>
+      <label>
+        <input type="checkbox" /> Library
+      </label>
+      <label>
+        <input type="checkbox" /> CSC
+      </label>
+      <label>
+        <input type="checkbox" /> School of Business
+      </label>
+      <label>
+        <input type="checkbox" /> School of Engineering
+      </label>
+      <label>
+        <input type="checkbox" /> School of Education
+      </label>
+      <label>
+        <input type="checkbox" /> School of Nursing
+      </label>
+      <label>
+        <input type="checkbox" /> College of Arts and Sciences
+      </label>
+    </div>
+  );
+}
 
- export default FilterBox;
+export default FilterBox;
