6f67bec41a083dba95117547f8441071de759644
Bryce Kwon <signer@brycekwon.dev>
2024-03-28 18:06:35 -0700

    Update frontend queries for new experiences model

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -73,8 +73,8 @@ const Search = () => {
         {experiences.map((post, index) => (
           <div className="box" key={index} onClick={() => openPopup(post)}>
             <div>
-              <p>{post.name.first} {post.name.last} ({post.contact.email})</p>
-              <p>{post.location.country}{post.location.region === null ? "" : ", " + post.location.region}</p>
+              <p>{post.name} ({post.email})</p>
+              <p>{post.location.country}{post.location.city === null ? "" : ", " + post.location.city}</p>
             </div>
             <div className="country-flag-container">
               <img src={getCountryFlag(post.location.country)} alt="Country Flag" className="country-flag" />
