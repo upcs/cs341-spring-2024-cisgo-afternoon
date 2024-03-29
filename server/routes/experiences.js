5bca80ffb926142166ee6124fcb9cde22b74a74c
Bryce Kwon <signer@brycekwon.dev>
2024-03-29 12:22:44 -0700

    Add verification middleware for protected routes
    Routes still up for change. Add the verifyToken middleware to any
    that require the extra step of administrator authentication.

--- server/routes/experiences.js
+++ server/routes/experiences.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 
-import { verifyToken } from '../middleware/verifyJWT.js';
 import * as controller from '../controllers/experiences.js';
+import verifyToken from '../middleware/verifyToken.js';
 
 const experiencesRouter = Router();
 
 experiencesRouter.get('/', controller.getExperiences);
 experiencesRouter.get('/:id', controller.searchExperienceById);
 
-experiencesRouter.post('/', controller.searchExperienceByParams);
-experiencesRouter.route('/x').post(verifyToken, controller.editExperience);
+experiencesRouter.post('/', controller.searchExperienceByParams); 
+experiencesRouter.post('/:id', verifyToken, controller.editExperience);
 experiencesRouter.post('/new', controller.addExperience);
 experiencesRouter.post('/edit/:id', controller.editExperience);
 
