d81f894537f8f16e997b2f8a6baf20412011b3d6
starrnakamitsu <nakamits26@up.edu>
2024-04-25 13:15:24 -0700

    about page text

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,8 +4,20 @@ import '../static/css/pages/About.css';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div class='about_container'>
+      <header class='about_header'>
+        <h1 class='about_title'>About Us</h1>
+      </header>
+      <main class='about_main'>
+        <p class='about_text'>
+          This mapping project was conceived as a way to share the good work that UP faculty, staff, and alumni have 
+          engaged in around the world. The map stores information about different projects, research, work and other 
+          activities that our community has completed or is currently engaged in. We hope that this map serves as a 
+          resource for our UP community to connect with one another, share expertise, and brainstorm future projects. 
+          See the CISGO website for information on all the activities of the Collaborative on International Studies and 
+          Global Outreach.
+        </p>
+      </main>
     </div>
   );
 }
