2c871c3255ebd850faa630540a31bcae1972bbe1
Bryce Kwon <signer@brycekwon.dev>
2024-04-28 11:27:01 -0700

    Cleanup server project structure

--- server/server.js
+++ server/server.js
@@ -12,19 +12,19 @@ import experiencesRouter from './routes/experiences.js';
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(compression());
-
+app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
-app.use(cookieParser());
-
+// defined routes
 app.use('/auth', authRouter);
 app.use('/experiences', experiencesRouter);
 
+// catch-all
 app.all('*', (req, res) => {
   res.status(404).json({
     message: "This path is not defined"
@@ -43,17 +43,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-async function main() {
-  // connect to experiences database
-  await mongoose
-  .connect(process.env.DATA_DB)
-  .then(() => console.log('Connected to data database'))
-  .catch((err) => console.log(err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+  console.log(`App is listening to port: ${PORT}`);
+  await mongoose.connect(process.env.DATA_DB)
+    .then(() => console.log(`Connected to database`))
+    .catch((err) => console.log(err));
+});
 
-  // startup server
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-  });
-}
-main();
